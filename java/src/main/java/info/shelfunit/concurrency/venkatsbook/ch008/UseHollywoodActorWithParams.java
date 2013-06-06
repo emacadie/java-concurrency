4178f1bca4c6087cc15d797b7531a8619fb9f55c
Eric MacAdie <Eric@MacAdie.net>
2013-06-05 21:18:41 -0500

    I cleaned up some stuff

--- java/src/main/java/info/shelfunit/concurrency/venkatsbook/ch008/UseHollywoodActorWithParams.java
+++ java/src/main/java/info/shelfunit/concurrency/venkatsbook/ch008/UseHollywoodActorWithParams.java
@@ -5,30 +5,38 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.actor.UntypedActorFactory;
+// import src.main.java.info.shelfunit.concurrency.venkatsbook.ch008.HollywoodActorWithParams;
 
 // from Programming Concurrency on the JVM by Venkat Subramaniam   
 
 public class UseHollywoodActorWithParams {
 
-    public static void main( final String[] args ) throws InterruptedException {
-	try {
-	ActorSystem system = ActorSystem.create("This-is-for-Tom-Hanks");
-	// final ActorRef tomHanks = system.actorOf(new Props(HollywoodActorWithParams.class), "tom-hanks");
-
-	final ActorRef tomHanks = system.actorOf(new Props(new UntypedActorFactory() {
+    // no exception handling. How would you test this?
+    public static ActorRef getNewHollywoodActorWithParams( ActorSystem system, final String theName, final String nameArg ) {
+	return system.actorOf(new Props(new UntypedActorFactory() {
 		public UntypedActor create() {
-		    return new HollywoodActorWithParams("Tom Hanks");
+		    return new HollywoodActorWithParams(theName);
 		}
-	    }), "myactor");
+	    }), 
+	    nameArg
+	);
+    } // end getNewHollywoodActorWithParams() 
 
-	tomHanks.tell("James Lovell", tomHanks);
-	Thread.sleep(1000);
-	tomHanks.tell( new StringBuilder("Politics"), tomHanks );
-	Thread.sleep(1000);
-	tomHanks.tell("Forrest Gump", tomHanks);
-	system.shutdown();
+    public static void main( final String[] args ) throws InterruptedException {
+	ActorSystem system = ActorSystem.create("This-is-for-Tom-Hanks");
+	try {
+	    
+	    final ActorRef tomHanks = getNewHollywoodActorWithParams( system, "Tom Hanks", "tom-hanks");
+	    tomHanks.tell("James Lovell", tomHanks);
+	    Thread.sleep(1000);
+	    tomHanks.tell( new StringBuilder("Politics"), tomHanks );
+	    Thread.sleep(1000);
+	    tomHanks.tell("Forrest Gump", tomHanks);
+
 	} catch (Exception e) {
 	    e.printStackTrace();
+	} finally {
+	    system.shutdown();
 	}
     } // end method main
 
