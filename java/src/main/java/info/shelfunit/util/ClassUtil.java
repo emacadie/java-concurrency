1ba753d31cc5244901ad93671e2e2ab564ce8f59
Eric MacAdie <Eric@MacAdie.net>
2013-07-07 18:34:46 -0500

    Still refactoring ClassUtil

--- java/src/main/java/info/shelfunit/util/ClassUtil.java
+++ java/src/main/java/info/shelfunit/util/ClassUtil.java
@@ -1,7 +1,5 @@
 package info.shelfunit.util;
 
-// import java.util.List;
-// import java.util.ArrayList;
 import java.util.Set;
 import java.util.LinkedHashSet;
 
@@ -12,19 +10,13 @@ public class ClassUtil {
     private Set< String > ancestors;
     
     private void setInterfaces( Class c_1 ) {
-	
 	Class ci[] = c_1.getInterfaces();
-	System.out.println( c_1.getName() + " implements " + 
-	    ci.length + " interfaces" 
-	);
 	for ( Class nextClass : ci ) {
 	    this.interfaces.add( nextClass.getName() );
-	    System.out.println("Adding " + nextClass.getName()); 
 	}
     } // setIterfaces() 
 
     private void setAncestors() {
-	System.out.println( "Looking for ancestors of " + o_1.getClass().getName() );
 	ancestors = new LinkedHashSet< String >();
 	boolean isObject = false;
 	this.setInterfaces( o_1.getClass() );
@@ -33,11 +25,7 @@ public class ClassUtil {
 	String className = "";
 	while ( !isObject  ) {
 	    this.setInterfaces( parent );
-	    if (parent.getName() != null) {
-		System.out.println( parent.getName() + " is not null"  );
-	    }
 	    
-	    System.out.println( "Parent is a " + parent.getName() );
 	    if ( parent.getName().equals( "java.lang.Object" ) ) {
 		isObject = true;
 	    }
@@ -45,7 +33,7 @@ public class ClassUtil {
 	    ancestors.add( className );
 	    try {
 		parent = Class.forName( className ).getSuperclass();
-	    } catch (Exception e) {
+	    } catch ( Exception e ) {
 		isObject = true;
 	    }
 	}
@@ -54,13 +42,8 @@ public class ClassUtil {
 
     public ClassUtil( Object o2 ) {
 	this.o_1 = o2;
-	// try {
 	interfaces = new LinkedHashSet< String >();
-	// this.setInterfaces();
 	this.setAncestors();
-	// } catch ( Exception e ) {
-	    // e.printStackTrace();
-	    // }
     }
     
     public boolean doesImplement( String interfaceName ) {
@@ -70,9 +53,8 @@ public class ClassUtil {
 	    return false;
 	}
     } // end implements
-
     
-    /* This will return true if an object is an instance of a class
+    /** This will return true if an object is an instance of a class
        specified by the argument given. The argument must be the 
        fully-qualified class name.
      */
