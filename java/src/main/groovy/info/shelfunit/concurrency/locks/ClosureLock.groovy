437ca8e0ca799f9f3faa060b6fcbca2007fb9a50
EMacAdie <EMacAdie@gmail.com>
2015-04-21 21:58:01 -0500

    I made some locks final

--- java/src/main/groovy/info/shelfunit/concurrency/locks/ClosureLock.groovy
+++ java/src/main/groovy/info/shelfunit/concurrency/locks/ClosureLock.groovy
@@ -4,17 +4,21 @@ import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
 
 class ClosureLock { 
-  def lockSomeCode( block ) { 
+    final Lock lock = new ReentrantLock()
     
-    Lock lock = new ReentrantLock()
-    try { 
-      println( "about to try to get the lock" )
-      if ( lock.tryLock() || lock.tryLock( 500, TimeUnit.MILLISECONDS ) ) {
-	block.call()
-      }
-    } finally {
-      println( "About to try to unlock" )
-      lock.unlock()
+    // prevent lock from being changed
+    def setLock( arg ) {}
+    
+    def lockSomeCode( block ) {
+        
+        try { 
+            println( "about to try to get the lock" )
+            if ( lock.tryLock() || lock.tryLock( 500, TimeUnit.MILLISECONDS ) ) {
+                block.call()
+            }
+        } finally {
+            println( "About to try to unlock" )
+            lock.unlock()
+        }
     }
-  }
 }
